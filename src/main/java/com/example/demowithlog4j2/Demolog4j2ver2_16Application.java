27b2bbaf667d07aae35c9e073da46f05a746e36a
paulvkitor <paul@k2io.com>
2022-03-08 23:50:22 -0500

    initial commit

---
+++ src/main/java/com/example/demowithlog4j2/Demolog4j2ver2_16Application.java
@@ -0,0 +1,13 @@
+package com.example.demowithlog4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demolog4j2ver2_16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demolog4j2ver2_16Application.class, args);
+	}
+
+}
